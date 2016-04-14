c5a945e0da5b8c800859a56ac8a152075e80a4c7
tomplays <root@debout.octopuce.fr>
2016-04-14 02:28:22 +0200

    home template (wip)

---
+++ wp-content/themes/nuitdebout/home.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+            <p>todo include index.html content</p>	
+	</main><!-- .site-main -->
+	<?php // get_sidebar( 'content-bottom' ); ?>
+</div><!-- .content-area -->
+<?php /// get_sidebar(); ?>
+<?php get_footer(); ?>
