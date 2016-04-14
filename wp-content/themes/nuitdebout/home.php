be1570b2b469888895621a3c46cd42a0337757eb
Tomplays <homeof+git@gmail.com>
2016-04-14 02:39:27 +0200

    modif sur les commentaires du code

--- wp-content/themes/nuitdebout/home.php
+++ wp-content/themes/nuitdebout/home.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
+ * The template for displaying homepage
  *
  * @package WordPress
  * @subpackage Twenty_Sixteen
