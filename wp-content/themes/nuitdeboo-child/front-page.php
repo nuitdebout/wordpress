00f4919b56e71e70829f0004344a71c817beed4a
dooke <me@duke.fr>
2016-04-16 15:30:20 +0200

    Modules ACF

--- wp-content/themes/nuitdeboo-child/front-page.php
+++ wp-content/themes/nuitdeboo-child/front-page.php
@@ -3,8 +3,18 @@
 front-page.php
 
 <?php
-/**
-* Testing the social include
-*/
-get_template_part('templates/social');
+
+	/**
+	 * Module homepage screen
+	 */
+	if ( get_field('homepage_module_screen', 'option') == 'oui' ) {
+		get_template_part('templates/module', 'screen');
+	}
+
+	/**
+	* Module social network
+	*/
+	if ( get_field('global_module_social', 'option') == 'oui' ) {
+		get_template_part('templates/social');
+	}
 ?>
---
+++ wp-content/themes/nuitdeboo-child/templates/module-screen.php
@@ -0,0 +1,6 @@
+<?php
+	if ( get_option('homepage_screen', 'option') ) {
+		// Temporary
+		print_r( get_option('homepage_screen', 'option') );
+	}
+?>
