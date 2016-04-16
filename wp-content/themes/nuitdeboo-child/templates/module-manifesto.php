fc5e82f0efa0277767dd295e96ebf5ce858f7370
tomplays <homeof@gmail.com>
2016-04-16 17:45:30 +0200

    if no page fix

--- wp-content/themes/nuitdeboo-child/templates/module-manifesto.php
+++ wp-content/themes/nuitdeboo-child/templates/module-manifesto.php
@@ -4,8 +4,13 @@ module "manifesto"
 */
 $page = get_page_by_title( 'Manifeste' );
 $include = get_pages('include='.$page->ID);
-$content = apply_filters('the_content',$include[0]->post_content);
-$title = apply_filters('the_title',$include[0]->post_title);
-echo '<h2>'.$title.'</h2>';
-echo $content;
+if($include[0]->post_title){
+	$content = apply_filters('the_content',$include[0]->post_content);
+	$title = apply_filters('the_title',$include[0]->post_title);
+	echo '<h2>'.$title.'</h2>';
+	echo $content;
+}
+else{
+	echo 'creer page nommée "Manifeste"';
+}
 ?>
