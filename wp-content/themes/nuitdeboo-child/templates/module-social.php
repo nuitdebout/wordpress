83983a3d644b6ae5bc85134eb40aaee7a9b084f2
Nicolas Djambazian <nicolas@djambazian.fr>
2016-04-17 01:13:08 +0200

    basic social network integration

--- wp-content/themes/nuitdeboo-child/templates/module-social.php
+++ wp-content/themes/nuitdeboo-child/templates/module-social.php
@@ -1,26 +1,30 @@
-<ul class="list-inline">
+<h2>Réseaux sociaux</h2>
+<div class="social-networks-section">
 	<?php
-	$sc = array(
-		'facebook',
-		'twitter',
-		'bambuser',
-		'youtube',
-		'instagram',
-		'tumblr',
-		'periscope',
-		'snapchat',
-		'scoopit',
-		'github'
+	$socials = array(
+		'facebook' => 'facebook',
+		'twitter' => 'twitter',
+		'bambuser' => 'ban',
+		'youtube' => 'youtube-play',
+		'instagram' => 'instagram',
+		'tumblr' => 'tumblr',
+		'periscope' => 'ban',
+		'snapchat' => 'snapchat-ghost',
+		'scoopit' => 'ban',
+		'github' => 'github'
 	);
-	foreach ( $sc as $value ) :
-		if( get_field('social_'.$value, 'option') ) : ?>
-			<li>
-				<a href="<?php echo get_field('social_'.$value, 'option'); ?>" target="_blank" class="social-icons facebook">
-					<i class="fa fa-<?php echo $value ?>"></i>
-				</a>
-			</li>
+	foreach ( $socials as $name => $icon ) :
+		if( get_field('social_'.$name, 'option') ) : ?>
+			<a href="<?php echo get_field('social_'.$name, 'option'); ?>" target="_blank"
+				 class="social-networks-section-item social-networks-section-item--<?= $name ?>">
+					<i class="social-networks-section-item__icon fa fa-<?= $icon ?>" ></i>
+					<div class="social-networks-section-item__name">
+						<?= $name ?>
+					</div>
+
+			</a>
 			<?php
 		endif;
 	endforeach;
 	?>
-</ul>
+</div>
