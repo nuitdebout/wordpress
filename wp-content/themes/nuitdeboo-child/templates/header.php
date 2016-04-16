a92eef02beb9fd3148cd14055b5da275287f2e42
dooke <me@duke.fr>
2016-04-16 14:40:40 +0200

    header sample

--- wp-content/themes/nuitdeboo-child/templates/header.php
+++ wp-content/themes/nuitdeboo-child/templates/header.php
@@ -1,12 +1,23 @@
+<!-- Static navbar -->
 <header class="banner">
-	<div class="container">
-		<a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-		<nav class="nav-primary">
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
 			<?php
 			if (has_nav_menu('primary_navigation')) :
 				wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
 			endif;
 			?>
-		</nav>
-	</div>
+		</div><!--/.nav-collapse -->
+	</div><!--/.container-fluid -->
+</nav>
 </header>
