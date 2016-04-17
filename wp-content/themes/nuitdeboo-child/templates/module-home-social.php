63d38d574d102a584cc6e9c99c36222f7ae62623
dooke <me@duke.fr>
2016-04-17 16:52:53 +0200

    Social on home

---
+++ wp-content/themes/nuitdeboo-child/templates/module-home-social.php
@@ -0,0 +1,71 @@
+<div id="socialhome">
+	<div class="container">
+		<div class="page-header">
+			<h2>R&eacute;seaux sociaux</h2>
+		</div>
+	</div>
+	<div class="socialonhome container-fluid">
+		<div class="row">
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons facebook">
+					<i class="fa fa-facebook"></i><br />Facebook
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons twitter">
+					<i class="fa fa-twitter"></i><br />Twitter
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons tumblr">
+					<i class="fa fa-tumblr"></i><br />Tumblr
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons instagram">
+					<i class="fa fa-instagram"></i><br />Instagram
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons youtube">
+					<i class="fa fa-youtube-play"></i><br />Youtube
+				</a>
+			</div>
+			<div class="socialsquare nuitdebout col-xs-6 col-sm-4 col-md-2">
+				<h3>Nuit debout <small>est partout</small></h3>
+			</div>
+
+
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons snapchat">
+					<i class="fa fa-snapchat-ghost"></i><br />Snapchat
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons periscope">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ic_periscope.svg" />
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons periscope">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ic_periscope.svg" />
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons periscope">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ic_periscope.svg" />
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons periscope">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ic_periscope.svg" />
+				</a>
+			</div>
+			<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
+				<a href="https://www.periscope.tv/RemyBuisine" target="_blank" class="social-icons periscope">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ic_periscope.svg" />
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
