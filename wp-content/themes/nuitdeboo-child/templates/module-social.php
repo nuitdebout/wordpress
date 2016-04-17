21d12f5d4fa14d47cfd09d43e14d0b34bba0ec30
Nicolas Djambazian <nicolas@djambazian.fr>
2016-04-17 12:18:35 +0200

    add nuitdebout tile and fix font

--- wp-content/themes/nuitdeboo-child/templates/module-social.php
+++ wp-content/themes/nuitdeboo-child/templates/module-social.php
@@ -1,27 +1,71 @@
 <h2>Réseaux sociaux</h2>
 <div class="social-networks-section">
 	<?php
+	// @todo move in a config file
 	$socials = array(
-		'facebook' => 'facebook',
-		'twitter' => 'twitter',
-		'bambuser' => 'ban',
-		'youtube' => 'youtube-play',
-		'instagram' => 'instagram',
-		'tumblr' => 'tumblr',
-		'periscope' => 'ban',
-		'snapchat' => 'snapchat-ghost',
-		'scoopit' => 'ban',
-		'github' => 'github'
+		'twitter' => [
+			'icon' => 'twitter',
+		],
+		'facebook' =>  [
+			'icon' => 'facebook',
+		],
+		'bambuser' =>  [
+			'icon' => 'ban',
+		],
+		'youtube' =>  [
+			'icon' => 'youtube-play',
+		],
+		'instagram' =>  [
+			'icon' => 'instagram',
+		],
+		'nuitdebout' => [
+			'url' => 'http://www.nuitdebout.fr',
+			'image' => 'logowhite.svg',
+			'title' => 'dans le monde',
+			'special' => true
+		],
+		'tumblr' =>  [
+			'icon' => 'tumblr',
+		],
+		'periscope' =>  [
+			'icon' => 'ban',
+		],
+		'snapchat' =>  [
+			'icon' => 'snapchat-ghost',
+		],
+		'scoopit' =>  [
+			'icon' => 'ban',
+		],
+		'github' =>  [
+			'icon' => 'github'
+		],
 	);
-	foreach ( $socials as $name => $icon ) :
-		if( get_field('social_'.$name, 'option') ) : ?>
-			<a href="<?php echo get_field('social_'.$name, 'option'); ?>" target="_blank"
-				 class="social-networks-section-item social-networks-section-item--<?= $name ?>">
-					<i class="social-networks-section-item__icon fa fa-<?= $icon ?>" ></i>
-					<div class="social-networks-section-item__name">
-						<?= $name ?>
-					</div>
 
+	function getSocialLink($socialConfig, $name) {
+		if ($socialConfig['url']) {
+			return $socialConfig['url'];
+		}
+
+		return get_field('social_'.$name, 'option');
+	}
+
+
+
+	foreach ( $socials as $name => $socialConfig ) :
+		if( get_field('social_'.$name, 'option') || $socialConfig['special'] ) : ?>
+			<a href="<?php echo getSocialLink($socialConfig, $name) ?>" target="_blank"
+				 class="social-networks-section-item social-networks-section-item--<?= $name ?>">
+
+		 		<?php if ($socialConfig['icon']) { ?>
+					<i class="social-networks-section-item__icon fa fa-<?= $socialConfig['icon'] ?>" ></i>
+				<?php } else if ($socialConfig['image']) { ?>
+					<img class="social-networks-section-item__image" alt="<?= $name ?>"
+							 src="<?= get_stylesheet_directory_uri() . '/assets/images/' . $socialConfig['image'] ?>" />
+				<?php } ?>
+
+				<div class="social-networks-section-item__name">
+					<?= $socialConfig['title'] ? $socialConfig['title'] : $name ?>
+				</div>
 			</a>
 			<?php
 		endif;
