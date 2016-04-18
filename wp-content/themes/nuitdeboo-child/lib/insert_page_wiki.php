cb4bc14eed906c97293effcf36989bb2cf022045
tomplays <homeof@gmail.com>
2016-04-18 12:21:47 +0200

    wiki sync. function

--- wp-content/themes/nuitdeboo-child/lib/insert_page_wiki.php
+++ wp-content/themes/nuitdeboo-child/lib/insert_page_wiki.php
@@ -1,15 +1,9 @@
 <?php
 
+if(isset($_GET["syncc"]) && $_GET["syncc"] == "secret" ) {
+  	insert_page_wiki();
 
-add_action('query_vars', 'register_my_query_vars' );
-function register_my_query_vars( $qvars ){
-  if($_GET["syncc"]) {
-  		insert_page_wiki();
-
-  }
 }
-
-
 
 function get_data($url) {
 	$ch = curl_init();
@@ -22,13 +16,11 @@ function get_data($url) {
 	return 	json_decode($data, true);
 
 }
-function get_page_by_name($pagename)
-{
+function get_page_by_name($pagename){
 	$pages = get_pages();
 	foreach ($pages as $page) if ($page->post_name == $pagename) return $page;
 	return false;
 }
-
 
 
 function insert_page_wiki(){
@@ -36,26 +28,33 @@ function insert_page_wiki(){
 
 	foreach ( $returned_content as $p ) :
 
-	print_r($p);
+		//print_r($p);
 		$t =wp_strip_all_tags($p['slug']);
-		$c =$p['slug'];
+		$c = '';
 		$links =$p['links'];
+		if($t == 'paris'){
+			$c = $c. ' <p><iframe style="margin-top:20px" width="100%" height="500px" frameBorder="0" src="https://umap.openstreetmap.fr/en/map/nuit-debout-paris-plan-de-la-republique_80910?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=databrowser&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/en/map/nuit-debout-paris-plan-de-la-republique_80910">See full screen</a></p>';
+		}
+		$c = $c. '<ul>';
+		foreach ( $links as $l ) :
+			$c = $c.'<li><a href="'.$l.'">'.$l.'</a></li>';
+		endforeach;
+		$c = $c.'</ul>';
+		$c = $c. '<p>Vous voulez compléter cette page, ajouter des liens ? Venez sur le <a href="'.$p['wiki_url'].'">WIKI</a></p>';
+		echo $c;
 
-				foreach ( $links as $l ) :
-					echo $l;
-					endforeach;
+		$page = get_page_by_name($t);
+		if (!empty($page)) {
 
-
-	$page = get_page_by_name($t);
-	if (!empty($page)) {
-
-	echo '<pre> no create '.$t.'<br/></pre>';
-
+		$my_post = array(
+		    'ID'           => $page->ID,
+		    'post_content' => $c
+		  );
+		//print_r($my_post);
+		// Update the post into the database
+	  wp_update_post( $my_post );
+		echo 'p updated';
 	} else {
-
-
-
-
 
 		$my_post = array(
 		  'post_title'    =>  $t,
@@ -63,11 +62,10 @@ function insert_page_wiki(){
 		  'post_status'   => 'publish',
 		  'post_author'   => 1,
 		  'post_type' => 'page',
-		   'post_parent' => 0
+		  'post_parent' => 0
 		);
-	echo 'create '.$t;
-	// Insert the post into the database
-	//wp_insert_post( $my_post );
+		echo 'create '.$t;
+		wp_insert_post( $my_post );
 	}
 	endforeach;
 }
