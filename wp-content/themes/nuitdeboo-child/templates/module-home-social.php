2287249b3d13ce808e6f0919c250b15af87ddd28
Nhacsam <nicolas@djambazian.fr>
2016-04-19 23:29:33 +0200

    integration of new social section

--- wp-content/themes/nuitdeboo-child/templates/module-home-social.php
+++ wp-content/themes/nuitdeboo-child/templates/module-home-social.php
@@ -6,89 +6,83 @@
         </div>
 		</div>
 	</div>
-	<div class="socialonhome">
-		<div class="row">
+	<div class="social-networks-section">
 			<?php
 				$sc = array(
-							'facebook' => array(
-								'key_' => 'facebook',
-								'name'=>'Facebook',
-								'icon' => 'fa fa-facebook'
-							),
 							'twitter'=> array(
-								'key_' => 'twitter',
 								'name'=>'Twitter',
 								'icon' => 'fa fa-twitter'
 							),
+							'facebook' => array(
+								'name'=>'Facebook',
+								'icon' => 'fa fa-facebook'
+							),
 							'bambuser'=> array(
-								'key_' => 'bambuser',
 								'name'=>'Bambuser',
 								'icon' => ''
 							),
 							'youtube'=> array(
-								'key_' => 'youtube',
 								'name'=>'Youtube',
 								'icon' => 'fa-youtube-play'
 							),
-							'reddit'=> array(
-								'key_' => 'reddit',
-								'name'=>'Reddit',
-								'icon' => 'fa'
-							),
-							'partout'=> array(
-								'key_' => 'partout',
-								'name'=>'<h3>Nuit debout <small>est partout</small></h3>',
-								'icon' => ''
-							),
-
 							'instagram'=> array(
-								'key_' => 'instagram',
 								'name'=>'Instagram',
 								'icon' => 'fa fa-instagram'
 							),
 							'tumblr'=> array(
-								'key_' => 'tumblr',
 								'name'=>'Tumblr',
 								'icon' => 'fa fa-tumblr'
 							),
 							'periscope'=> array(
-								'key_' => 'periscope',
 								'name'=>'Periscope',
 								'icon' => ''
 							),
 							'snapchat'=> array(
-								'key_' => 'snapchat',
 								'name'=>'Snapchat',
 								'icon' => 'fa fa-snapchat-ghost'
 							),
 							'scoopit'=> array(
-								'key_' => 'scoopit',
 								'name'=>'Scoopit',
 								'icon' => ''
 							),
 							'github'=> array(
-								'key_' => 'github',
 								'name'=>'Github',
 								'icon' => 'fa fa-github'
 							),
+							'reddit'=> array(
+								'name'=>'Reddit',
+								'icon' => 'fa'
+							),
+							'nuitdebout'=> array(
+								'icon' => '',
+								'name' => 'est partout',
+								'image' => 'logowhite.svg'
+							),
 				);
 
-			foreach ( $sc as $value  ) :
-				if( get_field('social_'.$value['key_'], 'option') ) : ?>
-					<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
-						<a href="<?php echo get_field('social_'.$value['key_'], 'option'); ?>" target="_blank" class="social-icons facebook">
-							<i class="fa fa-<?php echo $value['icon']; ?>"></i><br /><?php echo $value['name']; ?>
+			foreach ( $sc as $key => $socialConfig ) :
+				if( get_field('social_'.$key, 'option') ) : ?>
+					<a href="<?php echo get_field('social_'.$key, 'option'); ?>" target="_blank"
+						class="social-networks-section-item social-networks-section-item--<?php echo $key ?>">
+							<i class="social-networks-section-item__icon fa fa-<?= $socialConfig['icon'] ?>" ></i>
+							<div class="social-networks-section-item__name">
+								<?php echo $socialConfig['name']; ?>
+							</div>
 						</a>
-					</div>
 					<?php
 				endif;
-				if( $value['key_'] == 'partout') : ?>
-				<div class="socialsquare col-xs-6 col-sm-4 col-md-2">
-						<?php echo $value['name']; ?>
-				</div>
-				<?php endif;
-				endforeach;
+				if ($key === 'nuitdebout') {
+				?>
+					<div class="social-networks-section-item social-networks-section-item--<?php echo $key ?>">
+						<img class="social-networks-section-item__image" alt="<?php echo $key ?>"
+								 src="<?php echo get_stylesheet_directory_uri() . '/assets/images/' . $socialConfig['image'] ?>" />
+						<div class="social-networks-section-item__name">
+							<?php echo $socialConfig['name']; ?>
+						</div>
+					</div>
+				<?php
+				}
+			endforeach;
 			?>
-		</div>
 	</div>
 </div>
