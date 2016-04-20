5aefcb1f89a8e617d29645c73f998eaa2b20a718
Lisa Durand <lisa@superdudu.net>
2016-04-21 00:56:04 +0200

    style-css-page town

--- wp-content/themes/nuitdeboo-child/page-ville.php
+++ wp-content/themes/nuitdeboo-child/page-ville.php
@@ -8,9 +8,18 @@
  */
 ?>
 <div class="container">
-  <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/page', 'header'); ?>
-    <?php get_template_part('templates/content', 'page'); ?>
-  <p>Vous voulez compléter cette page, ajouter des liens ? Venez sur le <a href="https://wiki.nuitdebout.fr/wiki/Villes/Agen">wiki</a></p>
-  <?php endwhile; ?>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="text-center ">
+    <?php while (have_posts()) : the_post(); ?>
+      <?php get_template_part('templates/page', 'header'); ?>
+      <?php get_template_part('templates/content', 'page'); ?>
+    <p>Vous voulez compléter cette page, ajouter des liens ? <br><br><a class="btn btn-primary" href="https://wiki.nuitdebout.fr/wiki/Villes/Agen"> Venez sur lewiki</a></p>
+    <?php endwhile; ?>
+      </div>
+    </div>
+  </div>
 </div>
+
+
+
