ad62e23a79ab5b976dd37781c2685f861e9c3c50
tomplays <homeof@gmail.com>
2016-04-20 16:03:39 +0200

    page template / sync. wiki script

---
+++ wp-content/themes/nuitdeboo-child/page-globaldebout.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: page global debout
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+<div class="container">
+  <?php while (have_posts()) : the_post(); ?>
+    <?php get_template_part('templates/page', 'header'); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+   	<div class="col s12">
+	      <div class="center-block">
+	        <h3>more infos:</h3>
+	        <p>
+	            <a href="http://twitter.com/GlobalDebout">twitter.com/GlobalDebout</a>
+	        </p>
+	        <p>
+	        	<a href="https://www.facebook.com/events/254751298208004/">facebook.com/events/254751298208004/</a>
+	      	</p>
+	      	<p>
+	        	<a href="mailto:intnuitdebout@riseup.net">intnuitdebout@riseup.net</a>
+	      	</p>
+	      </div>
+    </div>
+  <?php endwhile; ?>
+</div>
--- wp-content/themes/nuitdeboo-child/page.php
+++ wp-content/themes/nuitdeboo-child/page.php
@@ -1,4 +1,6 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<div class="container">
+	<?php while (have_posts()) : the_post(); ?>
+	  <?php get_template_part('templates/page', 'header'); ?>
+	  <?php get_template_part('templates/content', 'page'); ?>
+	<?php endwhile; ?>
+</div>
