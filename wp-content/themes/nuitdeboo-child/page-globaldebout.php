6e7878f83e7118465fa1e7aa9ff62cbbe9e7c1e2
Lisa Durand <lisa@superdudu.net>
2016-04-21 21:43:15 +0200

    page global - page ville - reduce banner height

--- wp-content/themes/nuitdeboo-child/page-globaldebout.php
+++ wp-content/themes/nuitdeboo-child/page-globaldebout.php
@@ -11,31 +11,40 @@
 	get_template_part('templates/module', 'screen');
 ?>
 <div class="container">
-  <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/content', 'page'); ?>
-    	<div class="col s12 bg-grey share-btns">
-	     	<div class="center-block">
-   				<div class="fb-share-button" data-layout="button_count"></div>
-				<span class="wrap"><a href="https://twitter.com/share" class="twitter-share-button" data-via="globaldebout" data-hashtags="nuitdebout">Tweet</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-				</span>
-			</div>
-		</div>
-
-   	<div class="col s12">
-	      <div class="center-block bg-grey padded ">
-	        <h3>more infos:</h3>
-	        <p>
-	            <a href="http://twitter.com/GlobalDebout">twitter.com/GlobalDebout</a>
-	        </p>
-	        <p>
-	        	<a href="https://www.facebook.com/events/254751298208004/">facebook.com/events/254751298208004/</a>
-	      	</p>
-	      	<p>
-	        	<a href="mailto:intnuitdebout@riseup.net">intnuitdebout@riseup.net</a>
-	      	</p>
-	      	<?php edit_post_link('edit', '<p>', '</p>'); ?>
-	      </div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="padded">
+        <div class="post">
+          <?php while (have_posts()) : the_post(); ?>
+            <?php get_template_part('templates/content', 'page'); ?>
+        </div>
+      	<div class="share-btns">
+  	     	<div class="center-block">
+     				<div class="fb-share-button" data-layout="button_count"></div>
+  				<span class="wrap"><a href="https://twitter.com/share" class="twitter-share-button" data-via="globaldebout" data-hashtags="nuitdebout">Tweet</a>
+  				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+  				</span>
+  			 </div>
+  		  </div>
+      </div>
     </div>
-  <?php endwhile; ?>
+  </div>
 </div>
+
+<div class="center-block bg-grey padded ">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <h2>More infos</h2>
+        <ul class='list-unstyled'>
+          <li><a href="http://twitter.com/GlobalDebout">twitter.com/GlobalDebout</a></li>
+          <li><a href="https://www.facebook.com/events/254751298208004/">facebook.com/events/254751298208004/</a></li>
+          <li><a href="mailto:intnuitdebout@riseup.net">intnuitdebout@riseup.net</a></li>
+        </ul>
+        <?php edit_post_link('edit', '<p>', '</p>'); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <?php endwhile; ?>
--- wp-content/themes/nuitdeboo-child/page-ville.php
+++ wp-content/themes/nuitdeboo-child/page-ville.php
@@ -11,46 +11,64 @@
 <?php
 	get_template_part('templates/module', 'screen');
 ?>
+
 <div class="container">
   <div class="row">
 
     <div class="col-md-8 col-md-offset-2">
-      <div class="text-center post">
+      <div class="post padded-top">
    		<?php the_content(); ?>
-
-   		<p>Vous voulez compléter cette page, ajouter des liens ? <a class="btn btn-primary" href="https://wiki.nuitdebout.fr/wiki/Villes/<?php echo get_the_title(); ?>"> Venez sur le wiki</a></p>
-
       	<?php edit_post_link('edit', '<p>', '</p>'); ?>
       </div>
     </div>
+  </div>
+</div>
+<?php get_template_part('templates/module', 'home-social'); ?>
 
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="padded-top">
+        <h2 class="text-center">Agenda</h2>
+        <div class="agenda scroll">
+             <!-- <div class="cbpgmp cibulMap" data-oamp data-cbctl="27805494/8131954" data-lang="fr" ></div> -->
+            <iframe style="width:100%;" frameborder="0" scrolling="no" allowtransparency="allowtransparency" class="cibulFrame cbpgbdy" data-oabdy src="//openagenda.com/agendas/27805494/embeds/8131954/events"></iframe>
+
+          <div class="cbpgcl cibulCalendar" data-oacl data-cbctl="<?php echo get_field('agenda_page_id') ?>" data-lang="fr"></div>
+
+
+             <script type="text/javascript" src="//openagenda.com/js/embed/cibulCalendarWidget.js"></script>
+              <script type="text/javascript" src="//openagenda.com/js/embed/cibulBodyWidget.js"></script>
+              <script type="text/javascript" src="//openagenda.com/js/embed/cibulMapWidget.js"></script>
+        </div>
+      </div>
+
+    </div>
   </div>
 </div>
 
-
-
-
-        <div class="cbpgmp cibulMap" data-oamp data-cbctl="27805494/8131954" data-lang="fr" ></div>
-             <iframe style="width:100%;" frameborder="0" scrolling="no" allowtransparency="allowtransparency" class="cibulFrame cbpgbdy" data-oabdy src="//openagenda.com/agendas/27805494/embeds/8131954/events"></iframe>
-
-       <div class="cbpgcl cibulCalendar" data-oacl data-cbctl="<?php echo get_field('agenda_page_id') ?>" data-lang="fr"></div>
-
-
-        <script type="text/javascript" src="//openagenda.com/js/embed/cibulCalendarWidget.js"></script>
-         <script type="text/javascript" src="//openagenda.com/js/embed/cibulBodyWidget.js"></script>
-         <script type="text/javascript" src="//openagenda.com/js/embed/cibulMapWidget.js"></script>
-
-
-
-<?php get_template_part('templates/module', 'home-social'); ?>
-
-  <div class="row">
- 			 <div class="col-md-8 col-md-offset-2">
-      			<div class="">
-      			</div>
-      		</div>
+<div class="row">
+  <div class="col-md-6 col-md-offset-3">
+    <div class="padded text-center">
+      <h2>Contribuez</h2>
+      <p>Vous voulez compléter cette page, ajouter des liens ?</p>
+      <p><a class="btn btn-primary" href="https://wiki.nuitdebout.fr/wiki/Villes/<?php echo get_the_title(); ?>">Venez sur le wiki</a></p>
+    </div>
+  </div>
  </div>
 
-
+<div class="bg-grey">
+  <div class="row">
+   <div class="col-md-6 col-md-offset-3">
+     <div class="padded text-center">
+       <h2>Activez le site de votre ville</h2>
+       <p>Pour déployer un site de votre ville debout et le gérer comme vous le souhaitez :</p>
+       <p><a class="btn btn-primary" href="https://wiki.nuitdebout.fr/wiki/Villes/<?php echo get_the_title(); ?>">Faire une demande de site</a></p>
+      <p>Vous avez déja un site ? <a href="mailto:homeof+nb@gmail.com">Envoyez-nous</a> le lien</p>
+     </div>
+   </div>
+  </div>
+</div>
 
 <?php endwhile; ?>
+
