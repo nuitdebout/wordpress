d39b8a57083a392f595c8062e66f3c1d721bf591
tomplays <homeof@gmail.com>
2016-04-22 10:14:25 +0200

    page wrap

--- wp-content/themes/nuitdeboo-child/page.php
+++ wp-content/themes/nuitdeboo-child/page.php
@@ -1,6 +1,26 @@
+<?php
+	// get_template_part('templates/module', 'screen');
+?>
+<?php while (have_posts()) : the_post(); ?>
 <div class="container">
-	<?php while (have_posts()) : the_post(); ?>
-	  <?php get_template_part('templates/page', 'header'); ?>
-	  <?php get_template_part('templates/content', 'page'); ?>
-	<?php endwhile; ?>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="padded">
+        <div class="post">
+          <?php get_template_part('templates/page', 'header'); ?>
+
+            <?php get_template_part('templates/content', 'page'); ?>
+        </div>
+      	<div class="share-btns">
+  	     	<div class="center-block">
+     				<div class="fb-share-button" data-layout="button_count"></div>
+  				<span class="wrap"><a href="https://twitter.com/share" class="twitter-share-button" data-via="globaldebout" data-hashtags="nuitdebout">Tweet</a>
+  				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+  				</span>
+  			 </div>
+  		  </div>
+      </div>
+    </div>
+  </div>
 </div>
+ <?php endwhile; ?>
