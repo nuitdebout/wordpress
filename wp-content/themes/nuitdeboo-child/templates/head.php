74e0c968142afeada3b63c6aaa2ec19cdd3ff85f
tomplays <homeof@gmail.com>
2016-04-22 10:24:17 +0200

    og image path fix

--- wp-content/themes/nuitdeboo-child/templates/head.php
+++ wp-content/themes/nuitdeboo-child/templates/head.php
@@ -11,6 +11,6 @@
 	  <meta property="og:description" content="<?php bloginfo( 'description' ); ?> - Chaque jour, nous sommes des milliers à occuper l’espace public pour reprendre notre place dans la République. Rejoignez-nous, et décidons ensemble de notre devenir commun dans ce mouvement politique sans parti ni bannières.">
 	  <meta property="og:url" content="<?php bloginfo( 'url' ); ?>">
 	  <meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>">
-	  <meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/img/nuitdebout-thumb.png">
+	  <meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/nuitdebout-thumb.png">
 	  <script type="text/javascript" id="gretaScript" src="https://cdn.greta.io/greta.min.js" data-ac="db56c2f2489199823062893e30e03720"></script>
 </head>
