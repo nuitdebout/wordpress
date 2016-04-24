504451db26223daf8c503525026b5a5e3b315957
Alexandre Segura <mex.zktk@gmail.com>
2016-04-24 13:23:51 +0200

    Design agenda section.

--- wp-content/themes/nuitdeboo-child/templates/module-agenda.php
+++ wp-content/themes/nuitdeboo-child/templates/module-agenda.php
@@ -6,26 +6,17 @@
             Vous souhaitez compléter cet agenda, ajouter un évènement ?
             <a href="https://openagenda.com/nuitdebout">Venez sur l'agenda participatif Nuit Debout.</a>
         </p>
-
-
-        <div class="row">
-            <div class="cbpgmp cibulMap" data-oamp data-cbctl="27805494/8131954" data-lang="fr" ></div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3 col-md-offset-2">
-                <div class="cbpgcl cibulCalendar" data-oacl data-cbctl="27805494/8131954|fr" data-lang="fr" s></div>
-            </div>
-
-            <div class="col-md-6 col-md-offset-2">
-                <div class="agenda scroll">
-                    <iframe style="width:100%;" frameborder="0" scrolling="no" allowtransparency="allowtransparency" class="cibulFrame cbpgbdy" data-oabdy src="//openagenda.com/agendas/27805494/embeds/8131954/events"></iframe>
-                </div>
-            </div>
-
-            <script type="text/javascript" src="//openagenda.com/js/embed/cibulCalendarWidget.js"></script>
-            <script type="text/javascript" src="//openagenda.com/js/embed/cibulBodyWidget.js"></script>
-            <script type="text/javascript" src="//openagenda.com/js/embed/cibulMapWidget.js"></script>
-        </div>
     </div>
+</section>
+<div class="row">
+  <div class="col-md-3">
+      <div class="cbpgcl cibulCalendar" data-oacl data-cbctl="27805494/8131954|fr" data-lang="fr" s></div>
+      <div class="cbpgmp cibulMap hidden-sm hidden-xs" data-oamp data-cbctl="27805494/8131954" data-lang="fr" ></div>
+  </div>
+  <div class="col-md-9">
+      <iframe style="width:100%;" frameborder="0" scrolling="no" allowtransparency="allowtransparency" class="cibulFrame cbpgbdy" data-oabdy src="//openagenda.com/agendas/27805494/embeds/8131954/events?oaq[from]=<?php echo date('Y-m-d') ?>&oaq[to]=<?php echo date('Y-m-d') ?>"></iframe>
+  </div>
 </div>
+<script type="text/javascript" src="//openagenda.com/js/embed/cibulCalendarWidget.js"></script>
+<script type="text/javascript" src="//openagenda.com/js/embed/cibulBodyWidget.js"></script>
+<script type="text/javascript" src="//openagenda.com/js/embed/cibulMapWidget.js"></script>
