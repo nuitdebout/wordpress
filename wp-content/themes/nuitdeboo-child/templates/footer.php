e067f3297c43557e0ed0f29097d5a8b7ade3023d
tomplays <homeof@gmail.com>
2016-04-25 20:10:06 +0200

    footer link css.

--- wp-content/themes/nuitdeboo-child/templates/footer.php
+++ wp-content/themes/nuitdeboo-child/templates/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row foot">
 
-			<div class="col-xs-2 col-sm-4">
+			<div class="col-xs-12 col-sm-4">
 				<h5>#NuitDebout</h5>
 				<ul class="list-unstyled list-inline">
 				<?php get_template_part('templates/module', 'social'); ?>
@@ -10,33 +10,30 @@
 				<h5>Liens</h5>
 				<?php
 		        if (has_nav_menu('footer_navigation')) :
-		        	wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav nav-footer']);
+		        	wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'list-unstyled']);
 		        endif;
 		        ?>
 			</div>
 
-			<div class="col-xs-2 col-sm-4">
+			<div class="col-xs-12 col-sm-4">
 				<h5>Voir aussi</h5>
 				<a target="_blank" href="https://www.convergence-des-luttes.org/">
 					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/CONVERGENCE-DES-LUTTES.png"/>
 				</a>
-
 			</div>
 
-			<div class="col-xs-2 col-sm-4">
+			<div class="col-xs-12 col-sm-4">
 				<?php dynamic_sidebar('sidebar-footer'); ?>
 				<h5>Contribuer</h5>
 				<div class="contribute">
 					<ul class="social-networks list-inline">
 						<li>
-							<a href="http://wiki.nuitdebout.fr" target="_blank" class="social-icons social-icons-bigger wiki  ">
+							<a href="http://wiki.nuitdebout.fr" target="_blank" class="social-icons social-icons-bigger wiki">
 								<i class="ic-wiki_rounded"></i>
 							</a>
 						</li>
 						<li>
-							<a href="https://github.com/nuitdebout/" target="_blank" class="social-icons social-icons-bigger github  ">
-
-
+							<a href="https://github.com/nuitdebout/" target="_blank" class="social-icons social-icons-bigger github">
 								<i class="ic-github_rounded"></i>
 							</a>
 						</li>
