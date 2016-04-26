913d8612172568371522ec22aca3dc0fc1556f9d
tomplays <homeof@gmail.com>
2016-04-26 23:13:47 +0200

    widgets style and iframe fix

--- wp-content/themes/nuitdeboo-child/templates/module-free_iframe_2.php
+++ wp-content/themes/nuitdeboo-child/templates/module-free_iframe_2.php
@@ -2,8 +2,10 @@
     <?php
     $iframe = get_field('homepage_module_free_iframe_2', 'option');
     $iframe_height = get_field('homepage_module_free_iframe_2_height', 'option');
+    $iframe_id = '';
+    $iframe_class = 'snapwidget-widget';
     ?>
-    <div class="row">
-        <iframe src="https://snapwidget.com/in/?h=bnVpdGRlYm91dHxpbnwxODB8OHwyfHx5ZXN8NXxmYWRlT3V0fG9uU3RhcnR8bm98eWVz&ve=060416" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%;"></iframe>
+    <div class="row iframe-module iframe-module--2">
+        <iframe src="https://snapwidget.com/in/?h=bnVpdGRlYm91dHxpbnwxODB8OHwyfHx5ZXN8NXxmYWRlT3V0fG9uU3RhcnR8bm98eWVz&ve=060416"  height="<?php echo $iframe_height; ?>" class="<?php echo $iframe_class; ?>" allowTransparency="true" frameborder="0" scrolling="no" style="overflow:hidden;"></iframe>
     </div>
 <?php endif; ?>
