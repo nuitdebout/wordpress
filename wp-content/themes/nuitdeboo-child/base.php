48877c62e87a5996c2805fdcfab3b26265a0e3ce
tomplays <homeof@gmail.com>
2016-04-27 14:43:04 +0200

    piwik + GA + styles

--- wp-content/themes/nuitdeboo-child/base.php
+++ wp-content/themes/nuitdeboo-child/base.php
@@ -31,5 +31,29 @@ use Roots\Sage\Wrapper;
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
     </script>
+
+
+    <script>
+   	 	window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    	ga('create','UA-75928661-1','auto');ga('send','pageview')
+  	</script>
+  	<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+ 	<!-- Piwik -->
+	<script type="text/javascript">
+	  var _paq = _paq || [];
+	  _paq.push(['trackPageView']);
+	  _paq.push(['enableLinkTracking']);
+	  (function() {
+	    var u="//piwik.nuitdebout.fr/";
+	    _paq.push(['setTrackerUrl', u+'piwik.php']);
+	    _paq.push(['setSiteId', 4]);
+	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	  })();
+	</script>
+	<noscript><p><img src="//piwik.nuitdebout.fr/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
+	<!-- End Piwik Code -->
+
   </body>
 </html>
