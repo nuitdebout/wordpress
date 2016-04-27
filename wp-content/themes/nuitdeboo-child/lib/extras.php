8798dfeb113e741c7edb8762283f32b6886c80c8
tomplays <homeof@gmail.com>
2016-04-27 08:08:39 +0200

    compile iframe function and add dynamic class and ID

--- wp-content/themes/nuitdeboo-child/lib/extras.php
+++ wp-content/themes/nuitdeboo-child/lib/extras.php
@@ -2,6 +2,10 @@
 
 function nd_get_revolutionary_date()
 {
+	if(is_page() ){
+		return;
+	}
+
 	$days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
 
 	$start = new \DateTime('2016-03-31');
@@ -11,4 +15,22 @@ function nd_get_revolutionary_date()
 	$dayNumber = $diffDays + 31;
 
 	return sprintf('%s %s Mars', $days[$now->format('w')], $dayNumber);
+}
+
+
+function get_iframe($nid){
+	$output = '';
+	$prefix = 'homepage_module_free_iframe_';
+	if ( get_field($prefix.$nid, 'option') ) :
+		$iframe = array(
+	    		'url' 	 => get_field($prefix.$nid, 'option'),
+	   			'height' => get_field($prefix.$nid.'_height', 'option'),
+	   			'id'     => get_field($prefix.$nid.'_id', 'option'),
+	   			'class'  => get_field($prefix.$nid.'_class', 'option')
+	    );
+		$output .= '<div class="row iframe-module iframe-module--'.$nid.'">';
+		$output .=  '<iframe src="'.$iframe['url'].'"  height="'.$iframe['height'].'" class="'.$iframe['class'].'" id="'.$iframe['id'].'" allowTransparency="true" frameborder="0" scrolling="no"></iframe>';
+		$output .='</div>';
+	endif;
+    return $output;
 }
--- wp-content/themes/nuitdeboo-child/templates/module-free_iframe_1.php
+++ wp-content/themes/nuitdeboo-child/templates/module-free_iframe_1.php
@@ -1,15 +1,3 @@
-<?php if ( get_field('homepage_module_free_iframe_1', 'option') ) : ?>
-    <?php
-        $iframe = get_field('homepage_module_free_iframe_1', 'option');
-        $iframe_height = get_field('homepage_module_free_iframe_1_height', 'option');
-        $iframe_id = 'radio';
-        $iframe_class = '';
-        // get_field('homepage_module_free_iframe_1_height', 'option');
-
-    ?>
-
-    <div class="row iframe-module iframe-module--1">
-        <iframe id="<?php echo $iframe_id; ?>" height="<?php echo $iframe_height; ?>"  src="<?php echo $iframe; ?>"></iframe>
-    </div>
-
-<?php endif; ?>
+<?php
+   echo get_iframe(1);
+?>
