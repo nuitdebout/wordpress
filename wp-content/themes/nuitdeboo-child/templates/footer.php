8a8f69ee0cb38a262ea9a95d9277b88151f7786d
tomplays <homeof@gmail.com>
2016-04-28 08:36:31 +0200

    add a jquery target class, add title attr. for links

--- wp-content/themes/nuitdeboo-child/templates/footer.php
+++ wp-content/themes/nuitdeboo-child/templates/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row foot">
 
-			<div class="col-xs-12 col-sm-4">
+			<div class="foot-left col-xs-12 col-sm-4">
 				<h5>#NuitDebout</h5>
 				<ul class="list-unstyled list-inline">
 				<?php get_template_part('templates/module', 'social'); ?>
@@ -17,8 +17,8 @@
 
 			<div class="col-xs-12 col-sm-4">
 				<h5>Voir aussi</h5>
-				<a target="_blank" href="https://www.convergence-des-luttes.org/">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/CONVERGENCE-DES-LUTTES.png"/>
+				<a title="CONVERGENCE DES LUTTES" target="_blank" href="https://www.convergence-des-luttes.org/">
+					<img alt="CONVERGENCE DES LUTTES" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/CONVERGENCE-DES-LUTTES.png"/>
 				</a>
 				<?php dynamic_sidebar('sidebar-footer'); ?>
 			</div>
@@ -34,13 +34,13 @@
 				<div class="contribute">
 					<ul class="social-networks list-inline">
 						<li>
-							<a href="http://wiki.nuitdebout.fr" target="_blank" class="social-icons social-icons-bigger wiki">
-								<i class="ic-wiki_rounded"></i>
+							<a title="wiki"  href="http://wiki.nuitdebout.fr" target="_blank" class="social-icons social-icons-bigger wiki">
+								<i alt="wiki nuitdebout" class="ic-wiki_rounded"></i>
 							</a>
 						</li>
 						<li>
-							<a href="https://github.com/nuitdebout/" target="_blank" class="social-icons social-icons-bigger github">
-								<i class="ic-github_rounded"></i>
+							<a title="github" href="https://github.com/nuitdebout/" target="_blank" class="social-icons social-icons-bigger github">
+								<i alt="github" class="ic-github_rounded"></i>
 							</a>
 						</li>
 					</ul>
