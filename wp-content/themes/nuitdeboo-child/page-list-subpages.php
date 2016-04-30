1d6580f6b541c4e363cbdc3346962fcc27e3d230
Nicolas Djambazian <nicolas@djambazian.fr>
2016-04-30 15:37:10 +0200

    sort cities by first letter

--- wp-content/themes/nuitdeboo-child/page-list-subpages.php
+++ wp-content/themes/nuitdeboo-child/page-list-subpages.php
@@ -11,14 +11,32 @@
 <?php while (have_posts()) : the_post(); ?>
 
 <?php
-$tags = [];
+$tagsByLetter = [];
 if ($pages_sub = get_pages(['child_of' => $post->ID, 'post_type' => 'page', 'post_status' => 'publish'])) {
     foreach ($pages_sub as $p) {
         $content = apply_filters('the_content', $p->post_content);
-        $tags[] = [
-            'title' => apply_filters('the_title', $p->post_title),
+        $title = apply_filters('the_title', $p->post_title);
+        $item =  [
+            'title' => $title,
             'url' => esc_url(get_permalink($p->ID)),
         ];
+
+        if (count($pages_sub) < 20) {
+            if (! $tagsByLetter['']) {
+                $tagsByLetter[''] = [];
+            }
+            $tagsByLetter[''][] = $item;
+            continue;
+        }
+
+        $letter = $title[0];
+        // hack for the É
+        $letter = htmlentities($letter) === '' ? 'E' : $letter;
+
+        if (! $tagsByLetter[$letter]) {
+            $tagsByLetter[$letter] = [];
+        }
+        $tagsByLetter[$letter][] = $item;
     }
 }
 ?>
@@ -26,15 +44,21 @@ if ($pages_sub = get_pages(['child_of' => $post->ID, 'post_type' => 'page', 'pos
 <section class="section list-towns">
     <h2 class="section__title"><?php echo get_the_title() ?></h2>
     <div class="section__content">
-	    <?php if (!empty($tags)) : ?>
-			<ul class="tags-list">
-				<?php foreach ($tags as $tag) : ?>
-				<li class="tag">
-                    <a href="<?php echo $tag['url'] ?>"><?php echo $tag['title'] ?></a>
-                </li>
-				<?php endforeach; ?>
-			</ul>
-	    <?php endif; ?>
+        <?php if (!empty($tagsByLetter)) : ?>
+            <?php foreach ($tagsByLetter as $letter => $tags) : ?>
+                <?php if ($letter) : ?>
+                    <h3><?php echo $letter; ?></h3>
+                <?php endif; ?>
+
+                <ul class="tags-list">
+                    <?php foreach ($tags as $tag) : ?>
+                        <li class="tag">
+                            <a href="<?php echo $tag['url'] ?>"><?php echo $tag['title'] ?></a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </div>
 </section>
 
