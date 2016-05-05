71f6748ed59237506172e01cba17ba77793af2af
tomplays <homeof@gmail.com>
2016-05-06 00:55:05 +0200

    markup for categories/tags/media_category

--- theme/templates/entry-meta.php
+++ theme/templates/entry-meta.php
@@ -7,3 +7,18 @@
 <time class="updated" datetime="<?= get_post_time('c', true); ?>">
     <?= get_the_date(); ?>
 </time>
+<?php
+/*
+<div class="post-taxonomies">
+$taxonomies = array('category', 'post_tag','media_category');
+foreach( $taxonomies as $tax ) {
+	echo '<div class="post-taxonomy post-taxonomy--'.$tax.'">';
+	$categories = get_the_terms( $post->ID, $tax );
+	foreach( $categories as $category ) {
+	     echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a>';
+	}
+	echo '</div>';
+}
+</div>
+*/
+?>
