a0639a7ec17ca3cda766d5dae48efb7ac1989aa1
Nhacsam <nicolas@djambazian.fr>
2016-05-06 23:04:46 +0200

    Delete all the stuff

--- wp-content/plugins/enhanced-media-library/js/eml-media-grid.js
+++
@@ -1,8 +0,0 @@
-( function( $ ) {    
-    
-    $( document ).ready( function() {
-        
-        $('.media-toolbar.wp-filter .media-grid-view-switch').after( $('.media-toolbar.wp-filter .select-mode-toggle-button') ); 
-    });
-
-})( jQuery );
