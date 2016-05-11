bf82eaa7db01fe84861af809f1d85c805b354004
Atala <alois.guillope@gmail.com>
2016-05-11 20:16:05 +0200

    WIP

---
+++ python_scripts/main.py
@@ -0,0 +1,57 @@
+#!/bin/env/python3
+"""
+Exports all doleances as result.json from the wiki.
+
+Format is [{'date': [dol1, dol2]}]
+"""
+
+import re
+import json
+import requests
+from bs4 import BeautifulSoup as BS
+
+db = {}
+
+
+if __name__ == '__main__':
+
+    rep = requests.get('https://wiki.nuitdebout.fr/wiki/Villes/Paris/Cahiers_de_dol%C3%A9ances_et_d%27exigences/Dol%C3%A9ances')
+
+    soup = BS(rep.content,  "html.parser")
+    date = soup.find(class_='navbox collapsible noprint uncollapsed')
+    links = date.find_all('a')
+
+    for link in links:
+        url = 'https://wiki.nuitdebout.fr' + link['href']
+
+
+        # url = 'https://wiki.nuitdebout.fr/wiki/Villes/Paris/Cahiers_de_dol%C3%A9ances_et_d%27exigences/Dol%C3%A9ances/Dol%C3%A9ances_du_44_mars_(13_avril_2016)'
+
+        rep = requests.get(url)
+        soup = BS(rep.content, "html.parser")
+
+        title = soup.find(class_='mw-headline')
+
+        # filter links that are not doleance links
+        if title and url != 'https://wiki.nuitdebout.fr/wiki/Villes/Paris/Cahiers_de_dol%C3%A9ances_et_d%27exigences':
+            title = title.text
+            head = title[:21]
+            date = title[-14:-1]
+
+            text = soup.find(id='mw-content-text').text
+
+            dols = re.findall(r'\d{1}\s{0,1}[–|-](.*?)\d{0,1}\s{1}[–|-]', text, re.DOTALL|re.UNICODE)
+
+            nb_dols = len(dols)
+            last_dol = re.findall(str(nb_dols+1) + r'\s{0,1}[–|-](.*?) mDoléances du ...', text, re.DOTALL|re.UNICODE)
+
+            last_dol = last_dol[0].strip()
+            dols = [dol.strip() for dol in dols]
+            dols.append(last_dol)
+
+            db[date] = dols
+
+    with open('result.json', 'w') as f:
+        json.dump(db, f)
+
+    print(db)
---
+++ theme/lib/insert_doleances.php
@@ -0,0 +1,114 @@
+<?php
+
+// synchronize wiki and wordpress
+// curl data file
+// insert or create wp post
+// use secret key (wp-config.php)
+
+
+if(isset($_GET["sync"])) {
+        if($_GET["sync"] == SECRET_SYNC_WIKI ){
+            insert_page_wiki();
+        }
+        else{
+            echo 'false secret key';
+        }
+}
+
+function insert_page_wiki(){
+    $returned_content = get_data('https://raw.githubusercontent.com/nuitdebout/nuitdebout.github.io/master/data/cities.json');
+    $pageville = get_page_by_name('Liste des villes');
+
+    foreach ( $returned_content as $p ) :
+
+        //print_r($p);
+        $t = wp_strip_all_tags($p['name']);
+
+        $c = '';
+        $links =$p['links'];
+        if($t == 'paris'){
+            $c = $c. ' <p><iframe style="margin-top:20px" width="100%" height="500px" frameBorder="0" src="https://umap.openstreetmap.fr/en/map/nuit-debout-paris-plan-de-la-republique_80910?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=databrowser&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/en/map/nuit-debout-paris-plan-de-la-republique_80910">See full screen</a></p>';
+        }
+        $c = $c. '<ul>';
+        foreach ( $links as $l ) :
+            $c = $c.'<li><a href="'.$l.'">'.$l.'</a></li>';
+        endforeach;
+        $c = $c.'</ul>';
+        // echo $c;
+
+        $page = get_page_by_name($t);
+
+        if (!empty($page)) {
+
+        $keep_sync = get_post_meta( $page->ID, 'keep_sync',true);
+        echo $keep_sync;
+        if($keep_sync && $keep_sync == '1'){
+            $my_post = array(
+                'ID'         => $page->ID,
+                'post_content' => $c,
+                'post_parent' =>  $pageville->ID,
+                'post_title'  =>  $t
+            );
+
+
+                $post_id = wp_update_post( $my_post, true );
+                if (is_wp_error($post_id)) {
+                    $errors = $post_id->get_error_messages();
+                    foreach ($errors as $error) {
+                        echo $error;
+                    }
+                }
+                else{
+                            add_post_meta( $post_id, '_wp_page_template', 'page-ville.php' );
+                            echo $t.' updated <br/><br/>';
+
+                }
+            }
+            else{
+                echo $t.' not updated (keepsync off) <br/><br/>';
+            }
+
+        } else {
+
+        $my_post = array(
+          'post_title'    =>  $t,
+          'post_content'  => $c,
+          'post_status'   => 'publish',
+          'post_author'   => 1,
+          'post_type' => 'page',
+          'post_parent' =>  $pageville->ID,
+        );
+
+        $post_id =  wp_insert_post( $my_post,true );
+        if (is_wp_error($post_id)) {
+            $errors = $post_id->get_error_messages();
+            foreach ($errors as $error) {
+                echo $error;
+            }
+        }
+        else{
+            add_post_meta( $post_id, '_wp_page_template', 'page-ville.php' );
+        }
+
+        echo $t.' created <br/><br/>';
+
+    }
+    endforeach;
+}
+function get_data($url) {
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return  json_decode($data, true);
+
+}
+function get_page_by_name($pagename){
+    $pages = get_pages();
+    foreach ($pages as $page) if ($page->post_title == $pagename) return $page;
+    return false;
+}
+?>
