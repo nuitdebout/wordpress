a7474aa7daac3ff8776b9d143ddfa83e666b80a0
Alexandre Segura <mex.zktk@gmail.com>
2016-05-15 01:16:32 +0200

    Style homepage posts loop.

--- theme/index.php
+++ theme/index.php
@@ -1,4 +1,4 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php // get_template_part('templates/page', 'header'); ?>
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
