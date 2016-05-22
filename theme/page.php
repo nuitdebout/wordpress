0f8fd1a7f4c7746a72277172eab65844b827705d
Alexandre Segura <mex.zktk@gmail.com>
2016-05-23 01:59:18 +0200

    Structure for page, implement post thumbnail.

--- theme/page.php
+++ theme/page.php
@@ -1,19 +1,15 @@
-<?php while (have_posts()) : the_post();
+<?php use Roots\Sage\Titles; ?>
 
-if(get_field('page_include_screen') == 1){
-	get_template_part('templates/module', 'screen');
-}
-?>
-<section class="section">
-	<div class="section__content">
-		<div class="post">
-		 	<?php
-				if(get_field('page_include_screen') == 0) {
-					get_template_part('templates/page', 'header');
-				}
-				get_template_part('templates/content', 'page');
-			?>
-		</div>
+<?php while (have_posts()) : the_post(); ?>
+<article class="page">
+	<header class="page__header">
+		<h2><?= Titles\title(); ?></h2>
+		<?php if (has_post_thumbnail()) : ?>
+		<div class="page__thumbnail"><?php the_post_thumbnail() ?></div>
+		<?php endif; ?>
+	</header>
+	<div class="page__content">
+	<?php get_template_part('templates/content', 'page'); ?>
 	</div>
-</section>
+</article>
 <?php endwhile; ?>
