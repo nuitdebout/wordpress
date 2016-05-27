2c67d5b0452609cac742e6a633cc7ae25a883957
Fadomire <fadomire@gmail.Com>
2016-05-27 16:16:11 +0200

    remove streamdebout and tvdebout module

--- theme/front-page.php
+++ theme/front-page.php
@@ -33,7 +33,6 @@ else :
 		get_template_part('templates/module', 'home-social');
 	}
 	get_template_part('templates/module', 'rassemblements');
-	get_template_part('templates/module', 'youtube');
 
 	if ( !is_paged() &&get_field('homepage_module_free_iframe_2', 'option') ) {
 		get_template_part('templates/module', 'free_iframe_2');
