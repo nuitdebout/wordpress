4dd34479070b29533c64847f5d57a2d06d039c29
Alexandre Segura <mex.zktk@gmail.com>
2016-05-27 17:42:41 +0200

    Hide map on mobile.

--- theme/templates/module-rassemblements.php
+++ theme/templates/module-rassemblements.php
@@ -1,7 +1,7 @@
-<div id="rassemblements" class="section hidden">
+<div id="rassemblements" class="section hidden-xs hidden-sm">
 	<h2 class="section__title">Rassemblements</h2>
 </div>
-<div class="row" id="homepage-map">
+<div class="row hidden-xs hidden-sm" id="homepage-map">
 	<a class="primary-button homepage-map__button" href="<?php get_bloginfo('url')?>/ville/">Voir plus de villes ››</a>
 	<iframe border="none"  width="100%" height="400px" frameBorder="0" src="https://framacarte.org/fr/map/nuitdebout_2420?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=false&onLoadPanel=none&captionBar=false#5/46.332/3.340"></iframe>
 </div>
