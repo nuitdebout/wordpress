7da84e15f327f00e4414ab174f59456b5618ecec
Fadomire <fadomire@gmail.Com>
2016-05-29 18:46:00 +0200

    Fix openagenda sorting date

--- theme/templates/module-oaevent.php
+++ theme/templates/module-oaevent.php
@@ -1,4 +1,5 @@
 <?php
+use NuitDebout\Wordress\OpenAgenda;
 // Get event title & description in default language
 $event_title = current($event['title']);
 $event_description = current($event['description']);
@@ -8,7 +9,11 @@ $event_description = current($event['description']);
 	<div class="agenda__event-heading" role="tab" id="headingOne">
 	  	<div class="agenda__event-title">
 	        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#oa-event-<?php echo $event['uid'] ?>" aria-expanded="true" aria-controls="collapseOne" title="<?php echo $event_title ?>">
-				<strong><?php echo $event['firstTimeStart'] ?></strong> <?php echo wp_trim_words($event_title, 10) ?>
+				<strong>
+					<?php
+						echo OpenAgenda\get_next_timing($event, new \DateTime('now'))->format('H:i');
+					?>
+				</strong> <?php echo wp_trim_words($event_title, 10) ?>
 	        </a>
 	        <a role="button" class="pull-right" data-toggle="collapse" data-parent="#accordion" href="#oa-event-<?php echo $event['uid'] ?>" aria-expanded="true" aria-controls="collapseOne"><span class="glyphicon glyphicon-chevron-down"></span></a>
 	  	</div>
