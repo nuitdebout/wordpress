1b55a1ffed9e17021ba7bc5ef989873465417aa5
Nicolas Djambazian <nicolas@djambazian.fr>
2016-05-30 23:24:59 +0200

    fix the menu animation

--- theme/templates/header.php
+++ theme/templates/header.php
@@ -1,10 +1,9 @@
 <?php use NuitDebout\Wordress\OpenAgenda; ?>
 
-<?php if (is_main_site()) : ?>
-	<?php get_template_part('templates/module', 'current-actions'); ?>
-<?php endif ?>
-
-
 <header class="header">
+	<?php if (is_main_site()) : ?>
+		<?php get_template_part('templates/module', 'current-actions'); ?>
+	<?php endif ?>
+
 	<?php the_component('navbar'); ?>
 </header>
