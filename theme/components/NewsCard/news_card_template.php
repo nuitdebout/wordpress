212889ded61b41bd42b440aca27d62c8fe5016ca
Nicolas Djambazian <nicolas@djambazian.fr>
2016-06-01 23:33:30 +0200

    mobile news-card

--- theme/components/NewsCard/news_card_template.php
+++ theme/components/NewsCard/news_card_template.php
@@ -2,6 +2,7 @@
 	<?php echo $image ?>
 	<div class="news-card__caption">
 		<h4 class="news-card__title"><?php echo $title ?></h4>
+		<p class="news-card__content"><?php echo $content; ?></p>
 		<small class="news-card__source"><?php echo $source ?></small>
 	</div>
 	<p class="news-card__content-animated"><?php echo $content; ?></p>
